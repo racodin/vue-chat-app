72eaff0e4cd8f45d8c43827d2b093cd0c2711a60
racodin <skkim6.dev@gmail.com>
2020-01-17 10:09:46 +0900

    :sparkles: added props and data bind

--- src/components/ChatList.vue
+++ src/components/ChatList.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="chat-list">
     <ul>
-      <ChatListItem v-for="item in ([].lenght = 10)" :key="item.id" />
+      <li
+        v-for="data in dataset"
+        :key="data.id"
+      >
+        <ChatListItem 
+          :picture="data.picture"
+          :nickname="data.nickname"
+          :date="data.date"
+          :msg="data.msg"
+        />
+      </li>
     </ul>
   </div>
 </template>
@@ -12,13 +22,30 @@ export default {
   name: "ChatList",
   components: {
     ChatListItem
+  },
+  props: {
+    dataset: {
+      type: Array,
+      default(){
+        return [];
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.chat-list {
+  height: 300px;
+  overflow: hidden;
+  overflow-y: auto;
+  border: 1px solid #ccc;
+}
 ul {
   margin: 0;
   padding: 0;
+}
+li {
+  padding: 10px;
 }
 </style>
