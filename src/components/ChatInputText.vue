86f0cf43a9c71c8ce3db551cd32d1787ab02a26e
racodin <skkim6.dev@gmail.com>
2020-01-17 10:09:09 +0900

    :sparkles: added input submit feature

--- src/components/ChatInputText.vue
+++ src/components/ChatInputText.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
-    <input type="text" placeholder="Enter your message" />
+      <input 
+        type="text"
+        placeholder="Enter your message"
+        maxlength="100"
+        v-model="message"
+        @input="update($event.target.value)"
+        @keypress.enter="submit($event.target.value)"
+      />
   </div>
 </template>
 
 <script>
 export default {
-  name: "ChatInputText"
+  name: "ChatInputText",
+  data(){
+    return {
+      message: ""
+    }
+  },
+  methods: {
+    update(value){
+      this.$emit("update", value);
+    },
+    submit(value){
+      this.message = "";
+      this.$emit("submit", value);
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+input {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
