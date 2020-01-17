6d79575bd44f8dcd05464b83b7f475ccdf241f95
racodin <skkim6.dev@gmail.com>
2020-01-17 10:06:50 +0900

    :zap: added chatting data list

--- src/Chat.vue
+++ src/Chat.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="chat">
-    <ChatList />
-    <ChatInputText />
+    <ChatList :dataset="chatList"  />
+    <ChatInputText @submit="updateMessage" />
     <ChatInputEmoji />
   </div>
 </template>
@@ -17,8 +17,38 @@ export default {
     ChatList,
     ChatInputText,
     ChatInputEmoji
+  },
+  data(){
+    return {
+      chatList: [{
+        picture: "https://postfiles.pstatic.net/20160718_15/shmadle0510_1468805220543OHPbu_PNG/2016-07-18_10%3B25%3B29.PNG?type=w1",
+        name: "Nickname1",
+        date: "2020.01.18",
+        msg: "chatting message1"
+      }, {
+        picture: "",
+        name: "Nickname2",
+        date: "2020.01.19",
+        msg: "chatting message2"
+      }],
+      message: "Hi"
+    }
+  },
+  methods: {
+    updateMessage(value){
+      this.chatList.push({
+        picture: "",
+        name: "Nickname2",
+        date: "2020.01.19",
+        msg: value
+      })
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#chat {
+  width: 400px;
+}
+</style>
