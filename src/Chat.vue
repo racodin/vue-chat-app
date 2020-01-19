2116fb6db0cd32eac95fb0f4e18a5e2fac478148
racodin <skkim6.dev@gmail.com>
2020-01-19 17:20:32 +0900

    :hammer: added prototype and panel div

--- src/Chat.vue
+++ src/Chat.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="chat">
     <ChatList :items="items" :addItem="item" />
-    <ChatInputText :limit="limit" @update="updateMessage" @submit="submitMessage" />
-    <ChatInputLimit :length="count" :limit="limit" />
-    <ChatInputEmoji />
+    <div class="chat-panel">
+      <ChatInputText :limit="limit" @update="updateMessage" @submit="sendMessage" />
+      <ChatInputLimit :length="count" :limit="limit" />
+      <ChatInputEmoji />
+    </div>
   </div>
 </template>
 
 <script>
+import "@/utils/prototype";
 import ChatList from "@/components/ChatList";
 import ChatInputText from "@/components/ChatInputText";
 import ChatInputLimit from "@/components/ChatInputLimit";
@@ -41,12 +44,12 @@ export default {
     updateMessage(value) {
       this.count = value.length;
     },
-    submitMessage(value) {
+    sendMessage(value) {
       this.count = 0;
       this.item = {
         picture: "",
         name: "Nickname2",
-        date: "2020.01.19",
+        date: new Date().format("YYYY.MM.DD E a/p hh:mm"),
         message: value
       };
     }
@@ -59,5 +62,8 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
+}
+.chat-panel {
+  display: flex;
 }
 </style>
