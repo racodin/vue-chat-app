4178ec392e022331ab2a31975e37b6117e765882
racodin <skkim6.dev@gmail.com>
2020-02-04 11:29:01 +0900

    :hammer: modified sort feature of emoji list

--- src/components/ChatEmoji.vue
+++ src/components/ChatEmoji.vue
@@ -3,7 +3,8 @@
     <div class="emoji-tab">
       <a
         href=""
-        @click.prevent="selectTab(tab.name)"
+        :class="{ active: tab.name == category.name }"
+        @click.prevent="selectTab(tab)"
         v-for="tab in tabs"
         :key="tab.id"
         >{{ tab.icon }}</a
@@ -12,8 +13,8 @@
     <div class="emoji-list">
       <a
         href=""
-        @click.prevent="select(emoji)"
-        v-for="emoji in category"
+        @click.prevent="selectEmoji(emoji)"
+        v-for="emoji in category.emojis"
         :key="emoji.id"
         >{{ emoji }}</a
       >
@@ -40,142 +41,79 @@ export default {
     return {
       isDisplay: true,
       emojiList: [],
-      filterList: [],
-      category: [],
+      category: {},
       tabs: [
         {
           name: "Smileys & People",
-          icon: "😃"
+          icon: "😃",
+          range: /[\u{1F600}-\u{1F64F}\u{270A}-\u{270D}\u{1F441}-\u{1F47D}\u{1F900}-\u{1F937}\u{1F9D0}-\u{1F9DF}]/gu,
+          emojis: []
         },
         {
           name: "Animals & Nature",
-          icon: "🐻"
+          icon: "🐻",
+          range: /[\u{1F321}-\u{1F32C}\u{1F3D4}-\u{1F3D6}\u{1F408}-\u{1F43F}\u{1F940}\u{1F980}-\u{1F9AF}]/gu,
+          emojis: []
         },
         {
           name: "Food & Drink",
-          icon: "🍔"
+          icon: "🍔",
+          range: /[\u{1F336}-\u{1F378}\u{1F950}-\u{1F96F}\u{1F9C0}-\u{1F9CA}]/gu,
+          emojis: []
         },
         {
           name: "Activity",
-          icon: "⚽"
+          icon: "⚽",
+          range: /[\u{1F3C2}-\u{1F3CE}\u{1F938}-\u{1F93F}]/gu,
+          emojis: []
         },
         {
           name: "Travel & Places",
-          icon: "🌇"
+          icon: "🌇",
+          range: null,
+          emojis: []
         },
         {
           name: "Objects",
-          icon: "💡"
+          icon: "💡",
+          range: /[\u{2700}-\u{2704}\u{2706}-\u{2709}\u{270E}-\u{2712}\u{2728}\u{1F30D}-\u{1F31C}\u{1F37D}-\u{1F3AE}\u{1F3D7}-\u{1F3F7}\u{1F4A3}-\u{1F5FA}\u{1F941}-\u{1F94F}\u{1F9E0}-\u{1F9FF}\u{1F680}-\u{1F6FA}]/gu,
+          emojis: []
         },
         {
           name: "Symbols",
-          icon: "🔣"
+          icon: "🔣",
+          range: /[\u{2705}\u{2713}-\u{2727}\u{2729}-\u{27BF}]/gu,
+          emojis: []
         },
         {
           name: "Flags",
-          icon: "🎌"
+          icon: "🎌",
+          range: null,
+          emojis: []
         }
       ]
     };
   },
   mounted() {
     for (const value in new EmojiConvertor().data) {
-      this.emojiList.push(value);
-      // console.log(value);
-      // console.log(parseInt(value, 16));
-      // console.log(String.fromCodePoint(parseInt(value, 16)));
+      this.emojiList.push(String.fromCodePoint(parseInt(value, 16)));
     }
-    this.selectTab("Smileys & People");
+    this.sortEmoji();
+    this.selectTab(this.tabs[0]);
   },
   methods: {
-    select(item) {
-      this.$emit("select", item);
+    selectEmoji(emoji) {
+      this.$emit("select", emoji);
     },
-    selectTab(name) {
-      // 이모티콘 카테고리별로 어떻게 잘 나눌지 고민이 필요...
-      this.category = [];
-      switch (name) {
-        case "Smileys & People":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 128103 && decimal <= 128136) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 128513 && decimal <= 128591) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 129297 && decimal <= 129344) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 129489 && decimal <= 129504) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Animals & Nature":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 128001 && decimal <= 128064) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 129409 && decimal <= 129472) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 127793 && decimal <= 127811) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Food & Drink":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 127812 && decimal <= 127871) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Activity":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 127951 && decimal <= 127984) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 127992 && decimal <= 127993) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Travel & Places":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 128136 && decimal <= 128255) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Objects":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 127744 && decimal <= 127788) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 127872 && decimal <= 127937) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Symbols":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 9800 && decimal <= 9989) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            } else if (decimal >= 10067 && decimal <= 127569) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-        case "Flags":
-          this.emojiList.forEach(hex => {
-            let decimal = parseInt(hex, 16);
-            if (decimal >= 128001 && decimal <= 128064) {
-              this.category.push(String.fromCodePoint(parseInt(hex, 16)));
-            }
-          });
-          break;
-      }
+    sortEmoji() {
+      this.tabs.forEach(tab => {
+        this.emojiList.filter(emoji => {
+          if (emoji.match(tab.range)) tab.emojis.push(emoji);
+        });
+      });
+    },
+    selectTab(item) {
+      this.category = item;
     }
   }
 };
@@ -196,15 +134,15 @@ export default {
     line-height: 1.3rem;
     text-decoration: none;
     border-radius: 3px;
-    &:hover {
+    &:hover,
+    &.active {
       background-color: rgba(0, 0, 0, 0.2);
     }
   }
 }
 .emoji-list {
   position: relative;
-  min-height: 50px;
-  max-height: 100px;
+  height: 100px;
   padding: 10px;
   word-break: break-all;
   overflow: hidden;
